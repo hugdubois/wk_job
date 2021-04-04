6da077598cbb520afa530fa163cfd5e3652fe8b8
Hugues Dubois <hugdubois@gmail.com>
2021-04-04 02:23:13 +0200

    feat: add eye candy (physics based dragging item)

--- apps/wk_job_web/assets/js/components/DroppableApplicantsList.js
+++ apps/wk_job_web/assets/js/components/DroppableApplicantsList.js
@@ -2,34 +2,49 @@ import { Droppable, Draggable } from "react-beautiful-dnd"
 import PropTypes from "prop-types"
 import Applicant from "./Applicant"
 import ListTitle from "./ListTitle"
+import NaturalDragAnimation from "natural-drag-animation-rbdnd"
+
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "#eeeeee" : "#ffffff",
+})
 
 const DroppableApplicantsList = (props) => {
   return (
     <div className="list-container">
       <ListTitle title={props.title} count={props.items.length} />
       <Droppable droppableId={props.id}>
-        {(provided) => (
+        {(provided, snapshot) => (
           <ul
             className="list-content"
             {...provided.droppableProps}
             ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
           >
             {props.items.map((item, index) => (
               <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided) => (
-                  <li
-                    className="list-item"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    key={item.id}
+                {(provided, snapshot) => (
+                  <NaturalDragAnimation
+                    rotationMultiplier={3}
+                    style={provided.draggableProps.style}
+                    snapshot={snapshot}
                   >
-                    <Applicant
-                      name={item.name}
-                      description={item.description}
-                      thumb={item.thumb}
-                    />
-                  </li>
+                    {(style) => (
+                      <li
+                        className="list-item"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        key={item.id}
+                        style={style}
+                      >
+                        <Applicant
+                          name={item.name}
+                          description={item.description}
+                          thumb={item.thumb}
+                        />
+                      </li>
+                    )}
+                  </NaturalDragAnimation>
                 )}
               </Draggable>
             ))}
