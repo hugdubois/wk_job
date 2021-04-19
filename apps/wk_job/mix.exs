d1f523ac64fa8bf380c8f3d11d051882540e8b19
Hugues Dubois <hugdubois@gmail.com>
2021-04-19 07:56:07 +0200

    chore: ecto verison bump 3.4 => 3.6

--- apps/wk_job/mix.exs
+++ apps/wk_job/mix.exs
@@ -37,7 +37,7 @@ defmodule WkJob.MixProject do
   defp deps do
     [
       {:phoenix_pubsub, "~> 2.0"},
-      {:ecto_sql, "~> 3.4"},
+      {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.0"},
       # code quality only: [:dev] or [:dev, :test]
